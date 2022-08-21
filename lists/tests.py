699daedf3fefa85aef62d67c01923714f875d1ba
abdul halim <abdulhalim.cu10@gmail.com>
2022-08-22 01:17:11 +0600

    test multiple input using functional test

--- lists/tests.py
+++ lists/tests.py
@@ -20,15 +20,8 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
     
-    def test_home_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
-    
     # similar to the test_home_returns_correct_html() method
-    def test_home_page_returns_right_html(self):
+    def test_home_returns_correct_html(self):
         response = self.client.get('/')
 
         html = response.content.decode('utf8')
@@ -39,5 +32,10 @@ class HomePageTest(TestCase):
     
     def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
     
